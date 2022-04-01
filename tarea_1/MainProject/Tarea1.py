2a538e801b76ca24b1c77e00a8b280c23829d307
Kevin Campos Venegas <kenderman.8@gmail.com>
2022-04-01 17:39:21 -0300

    primer avance dia 01 04 2022

--- tarea_1/MainProject/Tarea1.py
+++ tarea_1/MainProject/Tarea1.py
@@ -1,56 +1,93 @@
 import os
 
 def main():
-    print("Iniciando...")
-    get_info()
+    menu()
+    textoPrincipal = "Hola Hola mi nombre es kevin"
+    print(get_cantidadDeLineas(textoPrincipal))
+    print(get_cantidadDePalabras(textoPrincipal))
+    print(get_cantidadDePalabrasNoRepetidas(textoPrincipal))
+    print(get_cantidadDeCaracteresConEspacio(textoPrincipal))
+    print(get_cantidadDeCaracteresSinEspacio(textoPrincipal))
+    makeSpace()
+    print(get_cantidadDeVecesQueSeRepitioUnaPalabra("Hola", textoPrincipal))
+    print(changeWordInText("kevin", "pablo", textoPrincipal))
+    
+# def get_info():
+#     #mainText = open('C:\Users\pc0128\Downloads\El_Arbol_De_La_Colina.txt')
+#     #type El_Arbol_De_La_Colina.txt | find /v /c ""
 
-def get_info():
-    #mainText = open('C:\Users\pc0128\Downloads\El_Arbol_De_La_Colina.txt')
-    #type El_Arbol_De_La_Colina.txt | find /v /c ""
+#     mainText = input("agrega tu texto: \n")
+#     allWords = mainText.split(" ")
+#     print(f"la cantidad de palabras es {len(allWords)}")
+#     conjuntoDePalabras = set(allWords)
+#     print(f"la cantidad de palabras no repetidas es {len(conjuntoDePalabras)}")
 
-    mainText = input("agrega tu texto: \n")
+#     print(f"la cantidad de palabras es {len(mainText)}")
+
+#     textoSinEspacio = mainText.replace(" ", "")
+#     print(f"la cantidad de palabras es {len(textoSinEspacio)}")
+
+"""
+número de líneas, OK
+número de palabras en total, OK
+número de palabras no repetidas, OK
+numero de caracteres con espacio, OK
+número de caracteres sin espacio. OK
+"""
+def makeSpace():
+    print("--------------------------------------------------------------------------------")
+
+def get_cantidadDeLineas(mainText):
+    allLines = mainText.split("\n")
+    return len(allLines)
+
+def get_cantidadDePalabras(mainText):
     allWords = mainText.split(" ")
-    print(f"la cantidad de palabras es {len(allWords)}")
+    return len(allWords)
+
+def get_cantidadDePalabrasNoRepetidas(mainText):
+    allWords = mainText.split(" ")
     conjuntoDePalabras = set(allWords)
-    print(f"la cantidad de palabras no repetidas es {len(conjuntoDePalabras)}")
+    return len(conjuntoDePalabras)
 
-    print(f"la cantidad de palabras es {len(mainText)}")
+def get_cantidadDeCaracteresConEspacio(mainText):
+    return len(mainText)
 
+def get_cantidadDeCaracteresSinEspacio(mainText):
     textoSinEspacio = mainText.replace(" ", "")
-    print(f"la cantidad de palabras es {len(textoSinEspacio)}")
+    return len(textoSinEspacio)
 
-def countWord(wordToFind, mainText):
-    count = 0
+def get_cantidadDeVecesQueSeRepitioUnaPalabra(wordToFind, mainText):
+    
+    count = get_cantidadDePalabras(mainText)
+    a = mainText.replace(wordToFind,"")
 
-    mainText = input("agrega tu texto: \n")
-    allWords = mainText.split(" ")
-    print(f"la cantidad de palabras es {len(allWords)}")
-    conjuntoDePalabras = set(allWords)
-    print(f"la cantidad de palabras no repetidas es {len(conjuntoDePalabras)}")
-
-    textoConLaPalabra = len(mainText)
-    textoSinLaPalabra = len( mainText.replace(wordToFind,"") )
+    print(a)
+    print(get_cantidadDePalabras(a))
+    
+    # count2 = get_cantidadDePalabras(textoSinLaPalabra)
+    # print( get_cantidadDePalabras(textoSinLaPalabra) )
 
     #hacer el substring por cada palabra
-
-    count = textoConLaPalabra - textoSinLaPalabra
-    return 0
+    count = count
+    return count
 
 def changeWordInText(wordToChange, newWorld, mainText):
-    mainText.replace(wordToChange, newWorld) #a =
-    return mainText
+    a = mainText.replace(wordToChange, newWorld) #a =
+    return a
 
 
 def menu():
-    pass
+    print("-----Bienvenido al menu de opciones-----")
+    while(True):
+        response = input("Que deseas hacer? \n 1.- Ver cantidad de lineas \n 2.- Ver cantidad de palabras \n 3.- Ver cantidad de palabras no repetidas \n 4.- Ver cantidad de caracteres con espacio \n 5.- Ver cantidad de caracteres sin espacio \n 6.- Ver cantidad de veces que se repite una palabra \n 7.- Cambiar una palabra en un texto \n 8.- Salir \n")
+        if(response == "1"):
+            print("seleccionaste la opcion 1")
+
+        if(response == "8"):
+            os.system("cls")
+            return
 
 if __name__=="__main__":
     main()
 
-"""
-número de líneas,
-número de palabras en total, 
-número de palabras no repetidas, 
-numero de caracteres conespacio, 
-número de caracteres sin espacio.
-"""
