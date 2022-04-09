904dbb050494ac4321ecc1e388a7150ef948eeb0
Kevin Campos Venegas <kenderman.8@gmail.com>
2022-04-08 22:35:53 -0400

    Finish 08 04 2022

--- tarea_1/MainProject/Tarea1.py
+++ tarea_1/MainProject/Tarea1.py
@@ -3,6 +3,7 @@
 #Copyright (c) 2020, Kevin C. Pablo B.
 
 # -*- coding: utf-8 -*-
+import string
 import sys
 import os
 
@@ -15,6 +16,8 @@ def main():
     path = "../archivosDeTesteo/Libros_txt_utf-8/"
     ruta = rutaFile()
     mainTextList.append( open(ruta).read() )
+
+    save_rute = "../archivosDeTesteo/Libros_txt_utf-8/output/"
 
     menu()
 
@@ -30,11 +33,17 @@ número de caracteres sin espacio. OK
 número de veces que se repite una palabra, OK
 
 cambiar una palabra en el texto, OK
+
+agregar la funcion de guardado, OK
+
+manejar erro tipo de dato erroneo, OK
 """
 
 # ｡☆✼★━━━━━━  FUNCIONES  ━━━━━━★✼☆｡
+
 def makeSpace():
     print("--------------------------------------------------------------------------------")
+
 def contectFila():
     pass
 def rutaFile():
@@ -105,6 +114,10 @@ def alert(text):
     print(f"└{fix} •✧✧• {fix}┘")
     print("")
 
+def guardarTxt(text, name):
+    with open(name, "w") as f:
+        f.write(text)
+
 def menu():
     
     mainTextList[0] = limpiarTexto(mainTextList[0])
@@ -118,9 +131,13 @@ def menu():
         print("｡☆✼★━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━★✼☆｡")
         print("")
 
-        response = input("Ingrese una opcion: ")
+        response = 1
+        while response != "1" and response != "2" and response != "3" and response != "4" and response != "5" and response != "6" and response != "7" and response != "8":
+            #alert("Ingrese una opcion valida")
+            response = input("Ingrese una opcion: ")
 
         os.system("cls")
+
         if(response == "1"):
             alert("la cantidad de lineas del texto es:")
             print(get_cantidadDeLineas(mainTextList[0]), "\n")
@@ -131,30 +148,53 @@ def menu():
 
         if(response == "3"):
             alert("la cantidad de palabras no repetidas es:")
-            print( get_cantidadDePalabrasNoRepetidas(mainTextList[0]) )
+            print( get_cantidadDePalabrasNoRepetidas(mainTextList[0]), "\n" )
 
         if(response == "4"):
             alert("la cantidad de caracteres con espacio es:")
-            print( get_cantidadDeCaracteresConEspacio(mainTextList[0]) )
+            print( get_cantidadDeCaracteresConEspacio(mainTextList[0]), "\n" )
         
         if(response == "5"):
             alert("la cantidad de caracteres sin espacio es:")
-            print( get_cantidadDeCaracteresSinEspacio(mainTextList[0]) )
+            print( get_cantidadDeCaracteresSinEspacio(mainTextList[0]), "\n" )
 
         if(response == "6"):
-            wordToFind = input("Ingrese la palabra a buscar: ")
-            alert("la cantidad de veces que se repite una palabra es:")
-            print( get_cantidadDeVecesQueSeRepitioUnaPalabra(wordToFind, mainTextList[0]) ) 
+            try:
+                wordToFind = input("Ingrese la palabra a buscar: ")
+                alert("la cantidad de veces que se repite la palabra es:")
+                print( get_cantidadDeVecesQueSeRepitioUnaPalabra(wordToFind, mainTextList[0]), "\n" )
+            except:
+                os.system("cls")
+                alert("Error, tipo de dato erroneo, vuelve a intentarlo")
 
         if(response == "7"):
-            wordToChange = input("Ingrese la palabra a cambiar: ")
-            newWorld = input("Ingrese la nueva palabra: ")
-            alert("el texto con la palabra cambiada es:")
-            print( changeWordInText(wordToChange, newWorld, mainTextList[0]) )
+            try:
+                wordToChange = input("Ingrese la palabra a cambiar: ")
+                newWorld = input("Ingrese la nueva palabra: ")
+                alert("el texto con la palabra cambiada es:")
+                a = changeWordInText(wordToChange, newWorld, mainTextList[0])
+                print( a , "\n")
+
+                response = input("quieres GUARDAR el txt? (si/no): ")
+                if(response == "si"):
+                    os.system("cls")
+                    
+                    #guardar
+                    guardarTxt(a, "outputText.txt")
+                    
+                    alert("El archivo se guardo en \PythonDataScience_Cource\ tarea_1\MainProject con el nombre de outputText.txt")
+                    os.system("pause")
+
+                os.system("cls")
+            except:
+                os.system("cls")
+                alert("Error, tipo de dato erroneo, vuelve a intentarlo")
+
+            
 
         if(response == "8"):
             os.system("cls")
-            alert("Saliendo...")
+            alert("Ejecucion terminada. Hasta luego...")
             os.system("pause")
             return
 
@@ -163,7 +203,8 @@ def menu():
             os.system("cls")
         else:
             os.system("cls")
-            alert("Ejecucion terminada. Hasta luego")
+            alert("Ejecucion terminada. Hasta luego...")
+            os.system("pause")
             return
 
 if __name__=="__main__":
